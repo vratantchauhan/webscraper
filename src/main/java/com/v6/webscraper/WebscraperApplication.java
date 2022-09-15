39318d25b2f89a316620684fc0821c7af4210e20
vratant <vratant.chauhan@gmail.com>
2022-09-14 19:48:36 -0500

    Link traversal for page 2 is working

--- src/main/java/com/v6/webscraper/WebscraperApplication.java
+++ src/main/java/com/v6/webscraper/WebscraperApplication.java
@@ -14,7 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,108 +29,95 @@ public class WebscraperApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(WebscraperApplication.class, args);
-		Document docu;
-		try{
-			docu = Jsoup.connect("https://www.mtsamples.com/site/pages/browse.asp?type=3-Allergy%20/%20Immunology").timeout(0).get();
-		}catch (HttpStatusException e){
-			System.out.println("http status exception, trying again.");
-			docu = Jsoup.connect("https://www.mtsamples.com/site/pages/browse.asp?type=3-Allergy%20/%20Immunology").timeout(0).get();
+		Path currentWorkingDir = Paths.get("").toAbsolutePath();
+		String filePath = currentWorkingDir+"src/main/resources/AllMedicalRecords.txt";
+		System.out.println(currentWorkingDir.normalize().toString());
+		try {
+			File file = new File("AllMedicalRecords.txt");
+			if (file.createNewFile()) {
+				System.out.println("File created: " + file.getName());
+			} else {
+				System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
 		}
-		String title = docu.title();
+		FileWriter fileWriter = new FileWriter("AllMedicalRecords.txt");
+		Document homePage;
+		try {
+			homePage = Jsoup.connect("https://mtsamples.com/").timeout(0).get();
+		} catch (HttpStatusException e) {
+			System.out.println("http status exception, trying again.");
+			homePage = Jsoup.connect("https://mtsamples.com/").timeout(0).get();
+		}
+		String title = homePage.title();
 		System.out.println(title);
-
-//		Connection connection = Jsoup.connect("https://www.mtsamples.com/site/pages/sample.asp?Type=5-Bariatrics&Sample=1061-Bariatric%20Consult%20-%20Surgical%20Weight%20Loss%20-%201");
-//		connection.timeout(0);
-//		Document docu2 = connection.get();
-//		Elements element = docu2.getElementsByClass("hilightBold");
-//		String content = element.text();
-//		System.out.println(content);
-//		MedicalTranscription medicalTranscription = formatText(content);
-//		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-//		String json = ow.writeValueAsString(medicalTranscription);
-//		System.out.println(json);
-//		System.out.println(medicalTranscription);
-
-//		Elements elements = docu.getElementsByClass("dropdown-menu");
-		for (Element a : docu.getElementsByTag("a")) {
-//			if (a.className().equalsIgnoreCase("dropdown-item")) {
-//				System.out.println(a.absUrl("href"));
-//			}
+		for (Element a : homePage.getElementsByTag("a")) {
 			for (Element parent : a.parents()) {
 				if (parent.tagName().equals("div") && parent.hasClass("collapse navbar-collapse")
 						&& a.className().equalsIgnoreCase("dropdown-item")) {
-					System.out.println("1"+a.absUrl("href"));
-
+					System.out.println("1" + a.absUrl("href"));
 					Connection connection = Jsoup.connect(a.absUrl("href"));
 					connection.userAgent("Mozilla/5.0");
 					connection.timeout(0);
-					Document docu2;
-					try{
-						docu2 = connection.get();
-					}catch (HttpStatusException e){
+					Document subPageUnderEachCategory;
+					try {
+						subPageUnderEachCategory = connection.get();
+					} catch (HttpStatusException e) {
 						System.out.println("http excp trying again.");
-						docu2 = connection.get();
+						subPageUnderEachCategory = connection.get();
 					}
-					for (Element a1 : docu2.getElementsByTag("a")) {
-//			if (a.className().equalsIgnoreCase("dropdown-item")) {
-//				System.out.println(a.absUrl("href"));
-//			}
+					for (Element a1 : subPageUnderEachCategory.getElementsByTag("a")) {
 						for (Element parent1 : a1.parents()) {
 							if (parent1.tagName().equals("div") && parent1.hasClass("collapse navbar-collapse")
 									&& a1.className().equalsIgnoreCase("dropdown-item")) {
-								System.out.println("2"+a1.attr("abs:href"));
-
-								Connection connection1 = Jsoup.connect("https://www.mtsamples.com/site/pages/sample.asp?Type=3-Allergy%20/%20Immunology&Sample=386-Allergic%20Rhinitis");
+								System.out.println("2" + a1.attr("abs:href"));
+								Connection connection1 = Jsoup.connect(a1.attr("abs:href"));
 								connection1.userAgent("Mozilla/5.0");
 								connection1.timeout(0);
-								Document docu3;
-								try{
-									docu3 = connection1.get();
-									Elements element = docu3.getElementsByClass("hilightBold");
+								Document finalPageForEachCategory;
+								try {
+									finalPageForEachCategory = connection1.get();
+									Elements element = finalPageForEachCategory.getElementsByClass("hilightBold");
 									String content1 = element.text();
-									System.out.println(content1);
+									System.out.println("3" + content1);
+									fileWriter.write(content1+"\n");
 //									MedicalTranscription medicalTranscription = formatText(content);
 //									ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 //									String json = ow.writeValueAsString(medicalTranscription);
 //									System.out.println(json);
 //									System.out.println(medicalTranscription);
-								}catch (HttpStatusException e){
+								} catch (HttpStatusException e) {
 									System.out.println("http excp trying again.");
-									docu3 = connection1.get();
-									Elements element = docu3.getElementsByClass("hilightBold");
+									finalPageForEachCategory = connection1.get();
+									Elements element = finalPageForEachCategory.getElementsByClass("hilightBold");
 									String content1 = element.text();
-									System.out.println(content1);
+									System.out.println("3" + content1);
+									fileWriter.write(content1+"\n");
 //									MedicalTranscription medicalTranscription = formatText(content);
 //									ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 //									String json = ow.writeValueAsString(medicalTranscription);
 //									System.out.println(json);
 //									System.out.println(medicalTranscription);
 								}
-//								Elements element = docu3.getElementsByClass("hilightBold");
+//								Elements element = finalPageForEachCategory.getElementsByClass("hilightBold");
 //								String content = element.text();
 //								MedicalTranscription medicalTranscription = formatText(content);
 //								ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
 //								String json = ow.writeValueAsString(medicalTranscription);
 //								System.out.println(json);
 //								System.out.println(medicalTranscription);
+								System.out.println("=============================================================================");
 							}
 						}
 					}
-//		Elements elements = docu.getElementsByTag("a");
-//		for(Element elements1 : elements){
-//			if (elements1.parent().tagName().equalsIgnoreCase("div") && elements1.parent().className().equalsIgnoreCase("dropdown-menu")){
-//				System.out.println(elements1.absUrl("href"));
-//			}
-//		}
-////		Elements allLinks = elements.tagName("a");
-//		for (Element link : allLinks){
-//			String absLink = link.absUrl("href");
-//			System.out.println(absLink);
-//		}
-
 				}
-			}}}
-	public static MedicalTranscription formatText(String content){
+			}
+		}
+	}
+
+	public static MedicalTranscription formatText(String content) {
 		MedicalTranscription medicalTranscription = new MedicalTranscription();
 		medicalTranscription.setMedicalSpecialty(content.substring(0, content.indexOf("SUBJECTIVE:")));
 		medicalTranscription.setSubjective(content.substring(content.indexOf("SUBJECTIVE:"), content.indexOf("MEDICATIONS:")));
